f5fca9474ae477ee2c5bc7741acbb26e323c2cf3
tattoolady13 <68519505+tattoolady13@users.noreply.github.com>
2026-05-22 06:21:24 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from “react”;
+import { useState, useEffect, useRef, useCallback } from "react";
 
 function createAudioContext() {
 return new (window.AudioContext || window.webkitAudioContext)();
